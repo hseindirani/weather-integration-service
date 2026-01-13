9e3d7692da5bc48f5648f18f189d56b519db43a4
hseindirani <dirani.hussein93@gmail.com>
2026-01-13 10:48:47 +0100

    Initial commit

---
+++ src/main/java/com/hussein/weather_integration_service/WeatherIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hussein.weather_integration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hussein/weather_integration_service/WeatherIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussein.weather_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
